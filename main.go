540424ef21159e8f8770d9abefe6e3b0c617c329
singhayushh <coding.ayush@gmail.com>
2020-09-01 01:50:26 +0530

    Server entry point setup

--- main.go
+++ main.go
@@ -5,15 +5,5 @@ import "boilerplate_golangfront/handlers"
 var server = handlers.Server{}
 
 func main() {
-
-	/*
-		var err error
-		err = godotenv.Load()
-		if err != nil {
-			log.Fatal("Error accessing environment variables")
-		}
-	*/
-
-	server.Init(":80")
-
+	server.Init(":8080")
 }
